6cd1396274553929e0cab91f5407008b3cdd2df0
xiantang <zhujingdi1998@gmail.com>
2020-06-29 22:42:02 +0800

    first commit

---
+++ src/test.kt
@@ -0,0 +1,69 @@
+import java.util.*
+
+/**
+ * @Author: xiantang
+ * @Date: 2020/6/28 22:28
+ */
+
+/**
+ * return type Unit
+ */
+
+
+//fun feedTheFish() {
+//    val day = randomDay()
+//    val food = "pellets"
+//    println ("Today is $day and the fish eat $food")
+//}
+//
+//fun randomDay(): String {
+//    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+//            "Friday", "Saturday", "Sunday")
+//    return week[Random().nextInt(week.size)]
+//}
+
+
+fun swim(speed: String = "fast") {
+    println("swimming $speed")
+}
+
+fun updateDirty(dirty: Int, operation: (Int) -> Int) {
+    val increaseDirty = operation(dirty)
+    println(increaseDirty)
+}
+
+fun increaseDirty( start: Int ) = start +1
+fun main(args:Array<String>) {
+//    feedTheFish()
+    val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+    val filtered = decorations.filter {
+        it[0] == 'p'
+    }
+    println("filtered: $filtered")
+    val newList = filtered.toList()
+    println("new list: $newList")
+
+    val lazyMap = decorations.asSequence().map {
+        println("access: $it")
+        it
+    }
+    println("lazy: $lazyMap")
+    println("-----")
+    println("first: ${lazyMap.first()}")
+    println("-----")
+    println("all: ${lazyMap.toList()}")
+    val lazyMap2 = decorations.asSequence().filter {it[0] == 'p'}.map {
+        println("access: $it")
+        it
+    }
+    var dirtyLevel = 20
+//    val waterFilter = { dirty : Int -> dirty / 2}
+//    println(waterFilter(dirtyLevel))
+//    val waterFilter : (Int) -> Int = { dirty ->dirty/2 }
+    val filter = decorations.filter {
+        it.contains("p")
+    }
+    println(filter)
+    updateDirty(1,::increaseDirty)
+}
+
